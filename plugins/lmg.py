e38b87902c7fa947f987c1cd12dfe4745d6b6075
DeOn <41499793+D30N@users.noreply.github.com>
2021-11-07 16:10:07 +0530

    Create lmg.py

---
+++ plugins/lmg.py
@@ -0,0 +1,18 @@
+# Custom plugin by @Deonnn 
+import os
+import asyncio
+import requests
+from userge import userge, Message, pool
+
+@userge.on_cmd("lmg", about={
+    'header': "Let me Google that for you real quick !!",
+    'usage': "{tr}lmg [query | reply to msg]"})
+async def lmg_(message: Message):
+    """lmg_"""
+    query = message.input_or_reply_str
+    if not query:
+        await message.err("Give something to LMG!")
+        return
+    query_encoded = query.replace(" ", "+")
+    lmg_url = f"http://google.com/search?q={query_encoded}"
+    await message.edit(f"**Okey, let me Google that for you...**\n👉 Click here: [{query}]({lmg_url})")

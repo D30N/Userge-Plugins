9e8f5e9c0fb5a27eca67f94e9a829a4d9c2dee3a
DeOn <41499793+D30N@users.noreply.github.com>
2021-11-11 13:44:07 +0530

    Update lmt.py

--- plugins/lmt.py
+++ plugins/lmt.py
@@ -7,8 +7,8 @@ from userge import userge, Message, pool
 @userge.on_cmd("lmt", about={
     'header': "Let me Search that for you real quick !!",
     'usage': "{tr}lmt [query | reply to msg]"})
-async def lmg_(message: Message):
-    """lmg_"""
+async def lmt_(message: Message):
+    """lmt_"""
     query = message.input_or_reply_str
     if not query:
         await message.err("Give something to LMT!")

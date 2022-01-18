b310afbd7dc6a4dfc7f441ae6f0718a2031280f5
DeOn <41499793+D30N@users.noreply.github.com>
2022-01-18 12:30:33 +0530

    Update logpms.py

--- plugins/logpms.py
+++ plugins/logpms.py
@@ -5,8 +5,10 @@ from pyrogram.errors import FloodWait
 
 PM_LOG = int(os.environ.get("PM_LOG", "-1001457141157"))
 
+DISALOW = [int(i) for i in os.environ.get("DISALOW", "777000 340489014").split(" ")]
 
-@userge.on_message(filters.private & filters.incoming & (filters.text | filters.audio | filters.document | filters.photo | filters.sticker | filters.voice | filters.video_note))
+
+@userge.on_message(filters.private & filters.incoming & ~filters.bot & ~filters.user(DISALOW))
 async def hlo(b, m):
     try: 
         await m.forward(

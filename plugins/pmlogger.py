4d60da59d04f430a359d2e6684fa9511171e32a8
DeOn <41499793+D30N@users.noreply.github.com>
2022-01-18 11:59:25 +0530

    Update pmlogger.py

--- plugins/pmlogger.py
+++ plugins/pmlogger.py
@@ -7,9 +7,7 @@ PM_LOG = int(os.environ.get("PM_LOG", "-1001457141157"))
 
 DISALOW = [int(i) for i in os.environ.get("DISALOW", "777000").split(" ")]  
 
-DISALOW2 = [int(i) for i in os.environ.get("DISALOW2", "340489014").split(" ")]
-
-@userge.on_message(filters.private & filters.incoming & (~filters.bot | ~filters.user(DISALOW) | ~filters.user(DISALOW2)))
+@userge.on_message(filters.private & filters.incoming & (~filters.bot | ~filters.user(DISALOW)))
 async def hlo(b, m):
     try: 
         await m.forward(
